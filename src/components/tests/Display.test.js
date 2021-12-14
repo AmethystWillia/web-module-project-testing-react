90fdae86cbfd22f8d1997d9c7781c7d48088f9e1
Amethyst Willia <zmithic@gmail.com>
2021-12-13 19:45:03 -0600

    Finished Display tests with MANY typos that I needed the reloution video's help to find

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -5,25 +5,48 @@ import userEvent from '@testing-library/user-event';
 
 import Display from './../Display';
 
+import mockFetchShow from '../../api/fetchShow';
+jest.mock('../../api/fetchShow');
+
+const testShow = {
+    name: 'Test',
+    summary: 'bla bla bla',
+    seasons: [
+        {id: 0, name: 'Season One', episodes: []},
+        {id: 1, name: 'Season Two', episodes: []},
+    ]
+};
 
 test('renders without errors with no props', () => {
     render (<Display />);
 });
 
-test('renders Show component when the button is clicked ', () => {
+test('renders Show component when the button is clicked ', async () => {
+    mockFetchShow.mockResolvedValueOnce(testShow);
     // Arrange
     render (<Display />);
     // Act
     const button = screen.getByRole('button');
     userEvent.click(button);
-
     // Assert
+    const showCont = await screen.findByTestId('show-container');
+    expect(showCont).toBeInTheDocument();
 });
 
-test('renders show season options matching your data when the button is clicked', () => {
-    expect(badVar).toBeInTheDocument();
+test('renders show season options matching your data when the button is clicked', async () => {
+    mockFetchShow.mockResolvedValueOnce(testShow);
+    // Arrange
+    render (<Display />);
+    // Act
+    const button = screen.queryByRole('button');
+    userEvent.click(button);
+    // Assert
+    await waitFor (() => {
+        const options = screen.queryAllByTestId('season-option');
+        expect(options).toHaveLength(2);
+    });
 });
 
-test('renders show season options matching your data when the button is clicked', () => {
-    expect(badVar).toBeInTheDocument();
-});
+// test('renders show season options matching your data when the button is clicked', () => {
+//     expect(badVar).toBeInTheDocument();
+// });
